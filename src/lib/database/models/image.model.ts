76a0b2af3b86557f2a5d2eb86aa1820fe3036484
Rohit <rohityadav787898@gmail.com>
2024-05-14 23:06:02 +0545

    MongoDB connection done and setup different models

---
+++ src/lib/database/models/image.model.ts
@@ -0,0 +1,72 @@
+import { Schema, models, model, Document } from "mongoose";
+export interface Image extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  securedUrl: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: string;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author?: { _id: string; firstName: string; lastName: string };
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ImageSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  transformationType: {
+    type: String,
+    required: true,
+  },
+  publicId: {
+    type: String,
+    required: true,
+  },
+  securedUrl: {
+    type: URL,
+    required: true,
+  },
+  width: {
+    type: Number,
+  },
+  height: {
+    type: Number,
+  },
+  config: {
+    type: Object,
+  },
+  transformationUrl: {
+    type: URL,
+  },
+  aspectRatio: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  prompt: {
+    type: String,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Image = models?.Image || model("Image", ImageSchema);
+export default Image;
