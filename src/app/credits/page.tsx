83b9efda50e989204d7eaebc3b5ef844503fd489
Rohit <rohityadav787898@gmail.com>
2024-05-14 12:49:03 +0545

    Added routing pages

---
+++ src/app/credits/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreditPage = () => {
+  return <div>Credits</div>;
+};
+
+export default CreditPage;
---
+++ src/app/transformations/[id]/update/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdataTransformationPage = () => {
+  return <div></div>;
+};
+
+export default UpdataTransformationPage;
