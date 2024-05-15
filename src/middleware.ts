0996b62d9ed8ddfbffb8121826ff411303e848cb
Rohit <rohityadav787898@gmail.com>
2024-05-15 15:26:01 +0545

    Done2...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks/clerk"],
+  publicRoutes: ["/api/webhooks/clerk"],
 });
 
 export const config = {
