d7a418b738a3bbbda727bcf04fb5029cac266b67
Rohit <rohityadav787898@gmail.com>
2024-05-15 15:33:56 +0545

    Again

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
-
+    const firstName = first_name || "";
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
